0036041708c624ef605edd7ffb197a054ab10b0a
Pham Quang Giap <giappham345@gmail.com>
2024-12-19 15:37:47 +0700

    WIP

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsDeleted { get; set; }
+    public List<string> Category { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+}
